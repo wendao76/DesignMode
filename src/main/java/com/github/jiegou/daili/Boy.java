799b1bacd74a2cf6c2fdaa8b21ac36b4ce487085
wendao76 <liwenhu@cyou-inc.com>
2019-12-14 23:36:33 +0800

    代理模式

---
+++ src/main/java/com/github/jiegou/daili/Boy.java
@@ -0,0 +1,11 @@
+package com.github.jiegou.daili;
+
+public class Boy implements IGiveFlower{
+    private Girl mm;
+    public Boy(Girl girl) {
+        this.mm = girl;
+    }
+    public void sendFlower() {
+        System.out.println(this.mm.name + "送你鲜花");
+    }
+}
---
+++ src/main/java/com/github/jiegou/daili/Girl.java
@@ -0,0 +1,8 @@
+package com.github.jiegou.daili;
+
+public class Girl {
+    public String name;
+    public Girl(String name){
+        this.name = name;
+    }
+}
